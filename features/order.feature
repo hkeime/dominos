fef8124975d5ea6084be2578009a8999fc1c04fa
hk <hkeime@gannett.com>
2015-01-09 14:52:05 -0600

    fixed broken tests and added steps to complete scenario for selecting carryout

--- features/order.feature
+++ features/order.feature
@@ -1,19 +1,28 @@
 Feature: Order Pizza Online From Dominos
 
-Scenario: Verify user can order online from Dominos homepage
+Scenario: Verify user can navigate to order page from homepage
   Given I am on the Dominos site
   When the homepage loads
   And I click order online link
   Then the order page displays
 
-Scenario: Verify user can select delivery or carryout
-  Given the delivery option is selected by default
+Scenario: Verify user can change order type from delivery to carryout
+  Given I am on the Order Online page
+  And the delivery option is selected
   When I click carryout button
   Then the carryout option is selected
-
-
-Scenario: Verify user can enter address and select a dominos location
+  When I enter a city
+  And I select a state
+  And I enter a zip code
+  And I click Continue
+  Then nearby dominos locations display
+  And I can click the button to order carryout
 
 Scenario: Verify user select order multiple pizzas
+  Given I have selected a carry out location
+  When I choose popular items
+  And I select a pizza
+  Then the cart is updated
+
 
 Scenario: Verify check out page loads
--- features/step_definitions/order_steps.rb
+++ features/step_definitions/order_steps.rb
@@ -2,31 +2,75 @@
 Given(/^I am on the Dominos site/) do
   visit 'https://order.dominos.com/en/'
 end
-
 When(/^the homepage loads/) do
     expect(page).to have_selector(:id, 'homePage')
+    #expect(page).to have_selector(:id, 'Dominos-HomePage')
 end
-
 And(/^I click order online link/) do
     click_link('Order Online')
 end
-
 Then (/^the order page displays/) do
     expect(page).to have_content("Is this order for delivery or carryout?")
 end
 
-#scenario2 delivery or carryout options
-Given(/^the delivery option is selected by default$/) do
-  expect(page).to have_css("#form__input--icon Delivery active")
-  #expect(page).to have_selector("input", :value =>"Delivery")
-#  expect(page).to have_selector("input",:value => 'Carryout')
 
+#scenario2 select carryout and enter address
+Given (/^I am on the Order Online page$/) do
+  visit 'https://order.dominos.com/en/pages/order/#/locations/search/'
 end
-
+And (/^the delivery option is selected$/) do
+  expect(page).to have_css('.Delivery.active')
+  expect(page).to have_css('.Carryout')
+  expect(page).to have_css('.Locations')
+end
 When(/^I click carryout button$/) do
-  find_button('Carryout').click
+  #require "debug"
+  find('.Carryout').click
+end
+Then(/^the carryout option is selected$/) do
+  expect(page).to have_no_css('.Delivery.active')
+  expect(page).to have_css('.Carryout.active')
+end
+When(/^I enter a city$/) do
+  #require "debug"
+  fill_in('City', :with => 'Prairieville')
 end
 
-Then(/^the carryout option is selected$/) do
-  pending
+When(/^I select a state$/) do
+  select('LA', :from => 'Region')
+end
+
+When(/^I enter a zip code$/) do
+  fill_in('Postal_Code', :with => 70769)
+end
+
+When(/^I click Continue$/) do
+  click_button('Continue')
+end
+
+Then(/^nearby dominos locations display$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I can click the button to order carryout$/) do
+  #expect(page).to have_css('.Based-On')
+  find('.based-on', :with => 'Nearby for Carryout / Pickup').visible?
+end
+
+
+#scenario4
+Given(/^I have selected a carry out location$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I choose popular items$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select a pizza$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the cart is updated$/) do
+  pending # express the regexp above with the code you wish you had
 end
